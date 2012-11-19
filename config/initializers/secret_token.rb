569f2f7e8cc882eccc4de2365e47b7a3ac4a8eb1
thisiswei <99plus2@gmail.com>
2012-11-19 12:11:02 -0800

    copying

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trevorvideo::Application.config.secret_token = '3cec4d833428f5d5fbbfff8f35690fbd70b4c6698eaa61cdfdeccc56c593686af7ab30aa62e90aafe78fe2437ecc12603cf25280b22c149815145fe84b18a7c3'
