2c18ee37ec09bf881739c07760d38827984dae02
thisiswei <99plus2@gmail.com>
2012-11-28 20:49:58 -0800

    showing staffpicks on frontpage

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,5 @@
 class VideosController < ApplicationController
   def index
-    @videos = Video.order('created_at DESC').includes(:first_person).where('first_person_id != 2').page(params[:page]).per_page(Video::PER_PAGE)
+    @videos = Video.order('created_at DESC').includes(:first_person).page(params[:page]).per_page(Video::PER_PAGE)
   end
 end
